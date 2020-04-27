dfd5f86271560ee4b0439edaa13e0046a7620b11
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-27 14:45:34 +0900

    update: carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'freemarketsample72e'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarketsample72e'
+  config.fog_directory  = 'freeeemarket3rd'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freeeemarket3rd'
 end

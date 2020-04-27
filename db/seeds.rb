c026c5df707277a901bf60278348a2b5dbd22da9
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-27 23:54:31 +0900

    update: brand

--- db/seeds.rb
+++ db/seeds.rb
@@ -489,11 +489,15 @@ conditions = Condition.create!([
   {name: "全体的に状態が悪い"},
 ])
 
-10.times do |i|
-  Bland.create!(
-    name: "Brand #{i+1}"
-  )
-end
+brands = Bland.create!([
+  {name: "adidas"},
+  {name: "Nike"},
+  {name: "Reebok"},
+  {name: "HERMES"},
+  {name: "エレコム"},
+])
+
+
 
 
 

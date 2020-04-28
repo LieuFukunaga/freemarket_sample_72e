52ad590ee0c88dfa5e6476651413e7b96930eb2a
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-28 14:54:28 +0900

    change: buy.js

--- app/assets/javascripts/buy.js
+++ app/assets/javascripts/buy.js
@@ -1,9 +1,8 @@
 $(function(){
-  // const KEY = gon.payjp_key;
+  const KEY = gon.payjp_key;
   $("button").click(function(e){
     e.preventDefault();
-    // Payjp.setPublicKey(KEY);
-    Payjp.setPublicKey("pk_test_ade9943c4f26730713dc9dfd");
+    Payjp.setPublicKey(KEY);
     var card = {
       cvc      : $("#cvc").val(),
       number   : $("#number").val(),

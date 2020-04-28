d24104dd05ffbcf0564e4e02737da6e5febcdebb
LieuFukunaga <heartarium01208.2@gmail.com>
2020-04-28 14:13:09 +0900

    refactoring: brand.js

--- app/assets/javascripts/brand.js
+++ app/assets/javascripts/brand.js
@@ -1,10 +1,8 @@
 $(function(){
   $(".brand-area").on("mouseenter", function(){
     $(".brands").removeClass("negative");
-    $("p.brand").removeClass("negative");
   });
   $(".brand-area").on("mouseleave", function(){
     $(".brands").addClass("negative");
-    $("p.brand").addClass("negative");
   });
 });
